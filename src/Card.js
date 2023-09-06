94d116de55460c44303be8aa22fc9ec639a67b83
CovenantJunior <covenanttope@gmail.com>
2023-09-06 15:39:56 +0100

    First structure

---
+++ src/Card.js
@@ -0,0 +1,40 @@
+import './Card.css';
+import clip from './clip.png';
+import indicator from './indicator.png';
+import logo from './logo.png';
+
+function Card() {
+  return (
+    <div className="card">
+      <h3 className="bank">bank</h3>
+      <img
+        src={clip}
+        alt="BIN Checker, Clip, Mastercard, VISA"
+        className="clip"
+      />
+      <img
+        src={indicator}
+        alt="BIN Checker, Indicator, Mastercard, VISA"
+        className="indicator"
+      />
+      <h3 className="number">5896 3456 0572 3698</h3>
+      <h5 className="card-holder">
+        <span>card holder</span>
+        <br />
+        Covenant T. Junior
+      </h5>
+      <h5 className="valid">
+        <span>expiry date</span>
+        <br />
+        01/27
+      </h5>
+      <img
+        src={logo}
+        alt="BIN Checker, Logo, Mastercard, VISA"
+        className="logo"
+      />
+    </div>
+  );
+}
+
+export default Card;
